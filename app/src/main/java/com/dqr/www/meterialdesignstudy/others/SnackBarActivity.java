835c64f2f7dcf2f787eb20e0c2f558c0efd2b289
bux-git <471025316@qq.com>
2017-07-17 16:39:02 +0800

    1.FloatingActionButton

--- app/src/main/java/com/dqr/www/meterialdesignstudy/others/SnackBarActivity.java
+++ app/src/main/java/com/dqr/www/meterialdesignstudy/others/SnackBarActivity.java
@@ -3,10 +3,10 @@ package com.dqr.www.meterialdesignstudy.others;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BaseTransientBottomBar;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 import com.dqr.www.meterialdesignstudy.R;
@@ -18,21 +18,21 @@ import com.dqr.www.meterialdesignstudy.R;
  */
 
 public class SnackBarActivity extends AppCompatActivity implements View.OnClickListener {
-    Button mBtnShow;
+    FloatingActionButton mFabShow;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.snackbar_layout);
-        mBtnShow = (Button) findViewById(R.id.btn_show);
+        mFabShow = (FloatingActionButton) findViewById(R.id.fab_show);
 
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.btn_show:
-               Snackbar snackbar= Snackbar.make(mBtnShow,"测试",Snackbar.LENGTH_LONG);
+            case R.id.fab_show:
+               Snackbar snackbar= Snackbar.make(mFabShow,"测试",Snackbar.LENGTH_LONG);
                 snackbar.setAction("编辑", new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
